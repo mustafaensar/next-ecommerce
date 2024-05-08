a71299292f1ee73e59bc0e64b4f547be1738ad31
Mustafa Ensar <mustafaensar55@gmail.com>
2024-05-08 18:20:34 -0500

    Fixed Comment component

--- app/components/detail/Comment.tsx
+++ app/components/detail/Comment.tsx
@@ -2,16 +2,20 @@
 
 import { RxAvatar } from "react-icons/rx"
 import Avatar from "../general/Avatar"
+import { Rating } from "@mui/material"
 
 const Comment = ({reviews}: {reviews : any}) => {
   
   console.log(reviews)
   
   return (
-    <div className="border w-full md:w-1/3">
-      <div className="flex items-center gap-1">
+    <div className="border w-full md:w-1/3 p-2 rounded-lg my-3">
+      <div className="w-[50px] flex items-center gap-1">
       <Avatar image={reviews?.user?.image}/>
-      <div>{reviews?.user?.name}</div>
+      <div>
+        <div>{reviews?.user?.name}</div>
+        <Rating name="read-only" value={reviews?.user?.rating} readOnly />
+      </div>
       </div>
       <div className="text-slate-500">
         {reviews?.comment}
